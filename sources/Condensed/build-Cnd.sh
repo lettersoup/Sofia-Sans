c8733442168d98c50fff0e18edd032e2ab8cd2ac
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2022-11-16 15:12:26 -0500

    Condensed build sh created

---
+++ sources/Condensed/build-Cnd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+#source ../venv/bin/activate
+
+echo "BUILDING SOFIA SANS CONDENSED FONTS"
+
+gftools builder sofia-cond.yml
+
+echo "Fonts Built!"
+
+echo "POST PROCESSING VFs"
+
+fonttools varLib.instancer ../../fonts/variable/Condensed/SofiaSansCondensed\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Condensed/SofiaSansCondensed\[wght\].ttf
+fonttools varLib.instancer ../../fonts/variable/Condensed/SofiaSansCondensed-Italic\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Condensed/SofiaSansCondensed-Italic\[wght\].ttf
+
+echo "DONE!"
