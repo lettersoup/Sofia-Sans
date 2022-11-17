1dd66f4a5dd3519143b2b9ccee40b2514ece2d0a
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2022-11-17 16:43:42 -0500

    SmiCond otf fonts added

--- sources/Semi-Condensed/build-SmiCnd.sh
+++ sources/Semi-Condensed/build-SmiCnd.sh
@@ -10,7 +10,16 @@ echo "Fonts Built!"
 
 echo "POST PROCESSING VFs"
 
-fonttools varLib.instancer ../../fonts/variable/Semi-Condensed/SofiaSansSemiCondensed\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Semi-Condensed/SofiaSansSemiCondensed\[wght\].ttf
-fonttools varLib.instancer ../../fonts/variable/Semi-Condensed/SofiaSansSemiCondensed-Italic\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Semi-Condensed/SofiaSansSemiCondensed-Italic\[wght\].ttf
+fonttools varLib.instancer ../../fonts/variable/Semi-Condensed/SofiaSansSemiCondensed\[wght\].ttf wght=100:900 --output ../../fonts/variable/Semi-Condensed/SofiaSansSemiCondensed\[wght\].ttf
+fonttools varLib.instancer ../../fonts/variable/Semi-Condensed/SofiaSansSemiCondensed-Italic\[wght\].ttf wght=100:900 --output ../../fonts/variable/Semi-Condensed/SofiaSansSemiCondensed-Italic\[wght\].ttf
+
+
+echo "POST PROCESSING OTFs"
+
+otfs=$(ls ../../fonts/otf/Semi-Condensed/*.otf)
+for otf in $otfs
+do
+    psautohint -x otilde,tildecomb,tilde $otf;
+done
 
 echo "DONE!"
