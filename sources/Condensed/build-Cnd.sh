fae8456b6d614174f90659947b2faf5bd4b39f5b
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2022-11-16 21:37:00 -0500

    Cond otf fonts added

--- sources/Condensed/build-Cnd.sh
+++ sources/Condensed/build-Cnd.sh
@@ -13,4 +13,13 @@ echo "POST PROCESSING VFs"
 fonttools varLib.instancer ../../fonts/variable/Condensed/SofiaSansCondensed\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Condensed/SofiaSansCondensed\[wght\].ttf
 fonttools varLib.instancer ../../fonts/variable/Condensed/SofiaSansCondensed-Italic\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Condensed/SofiaSansCondensed-Italic\[wght\].ttf
 
+
+echo "POST PROCESSING OTFs"
+
+otfs=$(ls ../../fonts/otf/Condensed/*.otf)
+for otf in $otfs
+do
+    psautohint $otf;
+done
+
 echo "DONE!"
