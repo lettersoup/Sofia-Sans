2d2ebe1ae7d691cf046385eeb1d8e7a7f27a6634
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2022-11-16 21:47:08 -0500

    ExtCnd otf fonts added

--- sources/Extra-Condensed/build-ExtCnd.sh
+++ sources/Extra-Condensed/build-ExtCnd.sh
@@ -13,4 +13,13 @@ echo "POST PROCESSING VFs"
 fonttools varLib.instancer ../../fonts/variable/Extra-Condensed/SofiaSansExtraCondensed\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Extra-Condensed/SofiaSansExtraCondensed\[wght\].ttf
 fonttools varLib.instancer ../../fonts/variable/Extra-Condensed/SofiaSansExtraCondensed-Italic\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Extra-Condensed/SofiaSansExtraCondensed-Italic\[wght\].ttf
 
+
+echo "POST PROCESSING OTFs"
+
+otfs=$(ls ../../fonts/otf/Extra-Condensed/*.otf)
+for otf in $otfs
+do
+    psautohint $otf;
+done
+
 echo "DONE!"
