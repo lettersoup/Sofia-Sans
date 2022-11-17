e4374cde215b1fe922f654a57de7300d953c66f2
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2022-11-16 22:31:01 -0500

    Normal otf fonts added

--- sources/Normal/build-Nrm.sh
+++ sources/Normal/build-Nrm.sh
@@ -13,4 +13,13 @@ echo "POST PROCESSING VFs"
 fonttools varLib.instancer ../../fonts/variable/Normal/SofiaSans\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Normal/SofiaSans\[wght\].ttf
 fonttools varLib.instancer ../../fonts/variable/Normal/SofiaSans-Italic\[wght\].ttf  wght=100:900 --output ../../fonts/variable/Normal/SofiaSans-Italic\[wght\].ttf
 
+
+echo "POST PROCESSING OTFs"
+
+otfs=$(ls ../../fonts/otf/Normal/*.otf)
+for otf in $otfs
+do
+    psautohint $otf;
+done
+
 echo "DONE!"
